81b3ba4ddd8f4e9667a44366bb7e52af01ad5303
Vatsal Gadhiya <vatsal.gadhiya@searce.com>
2022-11-08 11:56:21 +0530

    Add package

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # react-native-tidypos-terminal-noq
-React native stripe terminal
+
+A react-native library to make payments in sunmi device through tidypos gateway
+
+## Installation
+
+```sh
+npm install react-native-tidypos-terminal-noq
+```
+
+## Usage
+
+```js
+import TidyposTerminal from "react-native-tidypos-terminal-noq";
+
+// ...
+
+TidyposTerminal.startPayment(credentials, params)
+    .then((data) => {
+        // ...
+        console.log('success', data);
+    })
+    .catch((err) => {
+        // ...
+        console.log('error', err);
+    });
+```
+
+## Example
+1. Place your credentials in [example/src/constants.ts]
+2. `yarn example android`
+
+## Contributing
+
+See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
+
+## License
+
+MIT
---
+++ src/index.tsx
@@ -0,0 +1,37 @@
+import { NativeModules, Platform } from 'react-native';
+
+const LINKING_ERROR =
+  `The package 'react-native-tidypos-terminal-noq' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  '- You rebuilt the app after installing the package\n' +
+  '- You are not using Expo managed workflow\n';
+
+const TidyposTerminal = NativeModules.TidyposTerminal
+  ? NativeModules.TidyposTerminal
+  : new Proxy(
+      {},
+      {
+        get() {
+          throw new Error(LINKING_ERROR);
+        },
+      }
+    );
+
+function convertToString(params: any): any {
+  for (let key in params) {
+    if (params[key] !== null && params[key] !== undefined) {
+      params[key] = String(params[key]);
+    }
+  }
+  return params;
+}
+
+function test(params: any): Promise<any> {
+  return TidyposTerminal.test(convertToString(params));
+}
+
+function startPayment(credentials: string, params: any): Promise<any> {
+  return TidyposTerminal.startPayment(credentials, convertToString(params));
+}
+
+export default { startPayment, test };
